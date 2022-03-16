951770fbbd9323e57a3f25f89dbe0e76f6b71fe7
mpunuru <mallikarjuna.punuru@cyberark.com>
2022-03-16 14:13:25 +0530

    Added test cases

---
+++ CYBERARKIDENTITYSample/CAIdentity.swift
@@ -0,0 +1,22 @@
+//
+//  CAIdentity.swift
+//  CYBERARKIDENTITYSample
+//
+//  Created by Mallikarjuna Punuru on 16/03/22.
+//
+
+import Foundation
+
+public final class CAIdentity {
+
+    let name = "CAIdentity"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
