1cd1f7aa232f6b064d31f796cb0744dac953abf1
mpunuru <mallikarjuna.punuru@cyberark.com>
2022-03-16 14:04:29 +0530

    Initial project setup

---
+++ CYBERARKIDENTITYSample/CYBERARKIDENTITYSample.h
@@ -0,0 +1,18 @@
+//
+//  CYBERARKIDENTITYSample.h
+//  CYBERARKIDENTITYSample
+//
+//  Created by Mallikarjuna Punuru on 16/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CYBERARKIDENTITYSample.
+FOUNDATION_EXPORT double CYBERARKIDENTITYSampleVersionNumber;
+
+//! Project version string for CYBERARKIDENTITYSample.
+FOUNDATION_EXPORT const unsigned char CYBERARKIDENTITYSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CYBERARKIDENTITYSample/PublicHeader.h>
+
+
